89d993f5ae5a56ee32c01c04b15f1bed8a3509e2
BuildTools <unconfigured@null.spigotmc.org>
2022-10-08 08:44:24 -0700

    ud

--- scripts.js
+++ scripts.js
@@ -5,6 +5,14 @@ const signupScreen = document.querySelector('div.signup');
 
 var firstScreen = true;
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
 confirmB.onclick = () => {
     if (firstScreen == true) {
         firstScreen = false;
