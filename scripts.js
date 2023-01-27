fe6802ccb24fb5c4da0cc57d34eb620288f89064
Voltaicmimic431 <61982259+Voltaicmimic431@users.noreply.github.com>
2023-01-26 21:36:28 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,41 @@
 const bg_shape = document.getElementById('bg-shape')
-const typewrite = document.getElementById('typewrite')
+const back_to_top = document.body.querySelector('#back_to_top')
+const slide_in_objects = document.body.querySelectorAll('[content-slide-in]')
 
-const creator = "hello there"
+function stt() {setTimeout(function(){window.scrollTo(0, 0);}, 10)}
 
-// function _typewrite(creator_name) {
-//     let i = 0
-//     function _iterate () {
-//         setTimeout(function(){
-//             typewrite.innerHTML += creator_name.split('')[i]
-//             i++
-//             if (i < creator_name.length) {
-//                 _iterate()
-//             }
-//         }, 20)
-//     }
-
-//     _iterate()
-// } _typewrite(creator)
+function sts(section_num) {
+    sections = document.body.querySelectorAll('.section');
+    window.scrollTo({
+        left: 0,
+        top: section_num * window.innerHeight,
+        behavior: 'smooth'
+    });
+}
 
 function bg_shape_fade() {
     setTimeout(function() {
         bg_shape.style.animation = "5s ease-in-out 0s infinite alternate fadeinout"
     }, 3100)
 } bg_shape_fade()
+
+function check_LoS() {
+    let TH = (window.scrollY + window.innerHeight) - 20 // Threshold is set 20 pixel from bottom
+    slide_in_objects.forEach(obj => {
+        if (obj.clientTop < TH) {obj.classList.remove('hidden')}
+        else {console.log(obj); obj.classList.add('hidden')}
+    })
+
+    if (window.scrollY > window.innerHeight) {
+        back_to_top.classList.remove('sts_hidden')
+    } else {
+        back_to_top.classList.add('sts_hidden')
+        
+    }
+}
+
+slide_in_objects.forEach(obj => {
+    obj.classList.add('hidden')
+})
+
+document.addEventListener('scroll', check_LoS)
